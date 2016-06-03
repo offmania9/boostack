438cfad872ee2333e131fc5a2415c60065388565
Alessio Debernardi Venon <debbaccio22@gmail.com>
2016-06-03 12:16:11 +0200

    Updated Email_Basic class. Added Email_Mailgun class.

--- class/Email/Email_Basic.Class.php
+++ class/Email/Email_Basic.Class.php
@@ -1,113 +1,138 @@
 <?php
-
 /**
- * Boostack: Email_Basic.Class.php
+ * Boostack: Email.Class.php
  * ========================================================================
- * Copyright 2015-2016 Spagnolo Stefano
+ * Copyright 2015 Spagnolo Stefano
  * Licensed under MIT (https://github.com/offmania9/Boostack/blob/master/LICENSE)
  * ========================================================================
  * @author Spagnolo Stefano <s.spagnolo@hotmail.it>
- * @version 2.1
+ * @version 2
  */
-class Email_Basic
-{
+class Email_Basic {
 
     private $from_mail;
-
     private $from_name;
-
     private $reply_to_mail;
-
     private $reply_to_name;
-
     private $subject;
-
-    private $cc;
-
+    private $cc = array();
+    private $bcc = array();
     private $headers;
-
     private $attachment = array();
-
     private $message;
-
+    private $message_clean;
     private $date_send;
 
     private $to_list = array();
 
     private $mime_boundary;
 
-    public function __construct($from_mail, $message, $subject = "", $single_to = NULL, $from_name = "", $reply_to_mail = "", $reply_to_name = "", $cc = NULL)
-    {
-        $this->from_mail = $from_mail;
-        $this->from_name = $from_name;
-        $this->reply_to_mail = $reply_to_mail;
-        $this->reply_to_name = $reply_to_name;
-        $this->subject = $subject;
-        $this->cc = $cc;
-        
-        if ($single_to !== NULL) {
-            if (is_array($single_to)) {
-                foreach ($single_to as $v)
+    public function __construct($options) {
+
+        if(empty($options["from_mail"])) throw new Exception("Missing 'from_mail' parameter");
+        if(empty($options["message"])) throw new Exception("Missing 'message' parameter");
+        if(empty($options["to"])) throw new Exception("Missing 'to' parameter");
+
+        $this->from_mail = $options["from_mail"];
+        $this->from_name = !empty($options["from_name"]) ? $options["from_name"] : "";
+        $this->reply_to_mail = !empty($options["reply_mail"]) ? $options["reply_mail"] : "";
+        $this->reply_to_name = !empty($options["reply_name"]) ? $options["reply_name"] : "";
+        $this->subject = !empty($options["subject"]) ? $options["subject"] : "";
+        //$this->cc = $cc;
+        //$this->bcc = $bcc;
+        $cc = !empty($options["cc"]) ? $options["cc"] : NULL;
+        $bcc = !empty($options["bcc"]) ? $options["bcc"] : NULL;
+        $this->message_clean = $options["message"];
+
+        if($options["to"] !== NULL) {
+            if(is_array($options["to"]))
+                foreach($options["to"] as $v)
                     $this->to_list[] = $v;
-            } else
-                $this->to_list[] = $single_to;
+            else
+                $this->to_list[] = $options["to"];
         }
-        
+        if($cc !== NULL) {
+            if(is_array($cc))
+                foreach($cc as $v)
+                    $this->cc[] = $v;
+            else
+                $this->cc[] = $cc;
+        }
+        if($bcc !== NULL) {
+            if(is_array($bcc))
+                foreach($bcc as $v)
+                    $this->bcc[] = $v;
+            else
+                $this->bcc[] = $bcc;
+        }
+
         $semi_rand = md5(time());
         $this->mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
-        
-        $this->headers = "From: " . $this->from_mail;
-        $this->headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"" . $this->mime_boundary . "\"";
-        
-        $this->message = "This is a multi-part message in MIME format.\n\n" . "--" . $this->mime_boundary . "\n" . "Content-Type:text/html; charset=\"iso-8859-1\"\n" . "Content-Transfer-Encoding: 7bit\n\n" . $message . "\n\n";
+
+        $this->headers = "From: ".$this->from_mail;
+        $this->headers .= "\nMIME-Version: 1.0\n" .
+            "Content-Type: multipart/mixed;\n" .
+            " boundary=\"".$this->mime_boundary."\"";
+
+        $this->message = "This is a multi-part alert in MIME format.\n\n" .
+            "--".$this->mime_boundary."\n" .
+            "Content-Type:text/html; charset=\"iso-8859-1\"\n" .
+            "Content-Transfer-Encoding: 7bit\n\n" .
+            $this->message_clean ."\n\n";
+
     }
 
-    public function AddAddressToList($emailaddress)
-    {
+    public function AddAddressToList($emailaddress){
         $this->to_list[] = $emailaddress;
     }
 
-    public function addAttachment($path, $type)
-    {
+    public function addAttachment($path,$type){
+
         $this->attachment[] = $path;
-        $data = "";
-        $file = fopen($path, 'rb');
-        while (! feof($file)) {
+        $data ="";
+        $file = fopen($path,'rb');
+        while(!feof($file)) {
             $data .= fread($file, 2048);
         }
         fclose($file);
-        
-        $this->message .= "--" . $this->mime_boundary . "\n" . "Content-Type: " . $type . ";\n" . " name=\"" . basename($path) . "\"\n" . "Content-Disposition: attachment;\n" . " filename=\"" . basename($path) . "\"\n" . "Content-Transfer-Encoding: base64\n\n" . chunk_split(base64_encode($data)) . "\n\n";
+
+        $this->message .= "--".$this->mime_boundary."\n" .
+            "Content-Type: ".$type.";\n" .
+            " name=\"".basename($path)."\"\n" .
+            "Content-Disposition: attachment;\n" .
+            " filename=\"".basename($path)."\"\n" .
+            "Content-Transfer-Encoding: base64\n\n" .
+            chunk_split(base64_encode($data)) . "\n\n";
     }
 
-    public function Send()
-    {
-        $this->message .= "--" . $this->mime_boundary . "--\n";
+    public function Send(){
+        $this->message .= "--".$this->mime_boundary."--\n";
         $this->message = wordwrap($this->message, 70, "\r\n");
-        foreach ($this->to_list as $value) {
+        foreach($this->to_list as $value){
             $ok = mail($value, $this->subject, $this->message, $this->headers);
-            if (! $ok) {
-                // throw new Exception("Can't Send mail to: ".$value); exit("Can't Send mail to: ".$value);
+            if(!$ok){
+                #throw new Exception("Can't Send mail to: ".$value); exit("Can't Send mail to: ".$value);
                 return false;
             }
         }
         return true;
     }
 
-    public function __get($property_name)
-    {
-        if (isset($this->$property_name)) {
-            return ($this->$property_name);
-        } else {
-            return (NULL);
+    public function __get($property_name) {
+        if(isset($this->$property_name)) {
+            return($this->$property_name);
+        }
+        else{
+            return(NULL);
         }
     }
 
-    public function __set($property_name, $val)
-    {
+    public function __set($property_name, $val) {
+
         $this->$property_name = $val;
-        $sql = "UPDATE " . self::TABLENAME . " SET $property_name='" . $val . "'  WHERE id ='" . $this->id . "' ";
-        Database_PDO::getInstance()->query($sql);
+        #$sql = "UPDATE ".self::TABLENAME." SET $property_name='".$val."'  WHERE id ='".$this->id."' ";
+        #mysql_query($sql)or die (mysql_error().": $sql");
     }
+
 }
 ?>
---
+++ class/Email/Email_Mailgun.Class.php
@@ -0,0 +1,45 @@
+<?php
+
+require ROOTPATH .'vendor/autoload.php';
+//use Mailgun\Mailgun;
+
+class Email_Mailgun extends Email_Basic {
+
+    private $key = "YOUR-API-KEY";
+    private $domain = "YOUR-DOMAIN";
+
+    public function addAttachmentPath($path) {
+        $this->attachment = array_merge($this->attachment, array($path));
+    }
+
+    public function send(){
+        $mgClient = new Mailgun($this->key);
+        $domain = $this->domain;
+        $harr = array(
+            'from'    => $this->from_name.' <'.$this->from_mail.'>',
+            'subject' => $this->subject,
+            'html'    => '<html>'.$this->message_clean.'</html>' );
+
+        $attarr = array();
+        if(count($this->attachment)>0) {
+            $attarr["inline"] = $this->attachment;
+        }
+
+        foreach($this->to_list as $value){
+
+            $harr["to"] = $value;
+
+            if(count($this->cc)>0) {
+                $harr["cc"] = implode(", ", $this->cc);
+            }
+            if(count($this->bcc)>0) {
+                $harr["bcc"] = implode(", ", $this->bcc);
+            }
+
+            $result = $mgClient->sendMessage($domain, $harr, $attarr);
+        }
+        return true;
+    }
+
+}
+?>
