5f9c4114dc1d28fb379e5e8f76fa63c6361ca78a
Alessio Debernardi Venon <debbaccio22@gmail.com>
2017-02-07 17:15:26 +0100

    Updated registration with new users structure

--- registration.php
+++ registration.php
@@ -21,11 +21,11 @@ if(isset($_POST["email"]) && isset($_POST["psw1"]) && isset($_POST["psw2"])) {
     $psw1 = Utils::sanitizeInput($_POST["psw1"]);
     $psw2 = Utils::sanitizeInput($_POST["psw2"]);
     if(Utils::checkEmailFormat($email) && $psw1 === $psw2) {
-        $user = new User_Registration();
-        $arr["username"] = $email;
-        $arr["email"] = $email;
-        $arr["pwd"] = $psw1;
-        $user->insert($arr);
+        $user = new User();
+        $user->username = $email;
+        $user->email = $email;
+        $user->pwd = $psw1;
+        $user->save();
         require_once $boostack->registerTemplateFile("boostack/content_login_logged.phtml");
     }
     else
