0f41494d657781fb61223c3599cea4758f620e0b
stefano spagnolo <stefanospagnolo@MacBook-Pro-di-stefano-spagnolo.local>
2017-03-21 00:45:11 +0100

    add Seo improvements

--- assets/js/init.js
+++ assets/js/init.js
@@ -8,13 +8,11 @@ var initLibrary = function() {
 
     // Create your own modules in "/module" directory, then call them here
 
-    //if (getElementsByClassName("QuestionListInit").length) {
-        require(["module/exampleModule"], function (object) {
-            if (exampleModuleObject != null) return;
-            exampleModuleObject = new object();
-            exampleModuleObject.init();
-        });
-    //}
+    require(["module/exampleModule"], function (object) {
+        if (exampleModuleObject != null) return;
+        exampleModuleObject = new object();
+        exampleModuleObject.init();
+    });
 
     require(["module/cookieMessageModule"], function (object) {
         if (cookieMessageModule != null) return;
