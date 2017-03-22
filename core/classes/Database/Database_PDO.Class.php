9473bb946f1e38cfcb01607b381ffa075c5554b3
Riccardo Bertaglia <riccardo.bertaglia@augens.it>
2017-03-22 16:19:19 +0100

    driver pdo is now configurable in setup

--- core/classes/Database/Database_PDO.Class.php
+++ core/classes/Database/Database_PDO.Class.php
@@ -41,7 +41,7 @@ class Database_PDO
         try {
             if (self::$instance === null) {
                 Config::constraint("database_on");
-                self::$instance = new PDO('mysql:host=' . $host . ';dbname=' . $db, $username, $password, array(
+                self::$instance = new PDO(Config::get("driver_pdo").':host=' . $host . ';dbname=' . $db, $username, $password, array(
                     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
                 ));
                 self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
