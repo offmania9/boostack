cf59bdaeec3c8e38b5d621c646e8d6c9923437b4
Alessio Debernardi Venon <debbaccio22@gmail.com>
2017-05-03 23:31:41 +0200

    Updated d() and dd() helper functions

--- core/libs/helpers.php
+++ core/libs/helpers.php
@@ -1,14 +1,20 @@
 <?php
 
-function d($var) {
+function d() {
+    $args = func_get_args();
     echo "<pre>";
-    var_dump($var);
+    for ($i = 0; $i < count($args); $i++) {
+        var_dump($args[$i]);
+    }
     echo "</pre>";
 }
 
-function dd($var) {
+function dd() {
+    $args = func_get_args();
     echo "<pre>";
-    var_dump($var);
+    for ($i = 0; $i < count($args); $i++) {
+        var_dump($args[$i]);
+    }
     echo "</pre>";
     die();
 }
