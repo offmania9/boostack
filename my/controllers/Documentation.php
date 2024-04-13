9efeb6219a5326ac7665fd45aafbe5deafc0ab17
Stefano Spagnolo <>
2024-04-14 00:33:49 +0200

    add documentations by boostack version

--- my/controllers/Documentation.php
+++ my/controllers/Documentation.php
@@ -11,9 +11,34 @@ class Documentation extends \My\Controller
     public static function init()
     {
         parent::init();
-        View::render("documentation.phtml", array(
-            "canonical" =>  Request::getFriendlyUrl("documentation"),
+
+        $defaultVersion = "6.x";
+        $templatePath = "documentation_" . $defaultVersion . ".phtml";
+        $defaultUrl = Request::getFriendlyUrl("docs/" . $defaultVersion . "");
+        $currentVersion = $defaultVersion;
+        if (Request::hasQueryParam("version")) {
+            switch (Request::getQueryParam("version")) {
+                case "6.x": {
+                        $currentVersion = "6.x";
+                        break;
+                    }
+                case "5.0": {
+                        $currentVersion = "5.0";
+                        break;
+                    }
+                default: {
+                        Request::goToUrl($defaultUrl);
+                    }
+            }
+            $templatePath = "documentation_" . $currentVersion . ".phtml";
+        } else {
+            Request::goToUrl($defaultUrl);
+        }
+
+        View::render($templatePath, array(
+            "canonical" =>  Request::getFriendlyUrl("docs/" . $currentVersion),
             "pageTitle" => Language::getLabel("navigation.documentation"),
+            "currentVersion" => $currentVersion
         ));
     }
 }
